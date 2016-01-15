d18147a3f8453203af62cdcd6eec19feb1314975
DatPixelDev <vahidyandy@gmail.com>
2016-01-14 18:36:15 -0800

    Update DamageEvent.java

--- src/me/pyxled/combattag/events/DamageEvent.java
+++ src/me/pyxled/combattag/events/DamageEvent.java
@@ -2,7 +2,6 @@ package me.pyxled.combattag.events;
 
 import me.pyxled.combattag.Core;
 import me.pyxled.combattag.SettingsManager;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,140 +15,143 @@ import org.bukkit.event.entity.EntityDamageEvent;
 public class DamageEvent implements Listener {
 
     SettingsManager s = SettingsManager.getInstance();
-    Core core = new Core();
 
+    @SuppressWarnings("static-access")
     @EventHandler
     public void onDamage(EntityDamageEvent e){
         EntityDamageEvent.DamageCause c = e.getCause();
         if(e.getEntity().getType() == EntityType.PLAYER){
             Player p = (Player)e.getEntity();
-            if(!core.combatTagTime.containsKey(p)) {
+            if(p.hasPermission("act.bypass")){
+                return;
+            }
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) {
                     if (s.getConfig().getBoolean("config.CombatTriggers.BlockDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.DROWNING) {
                     if (s.getConfig().getBoolean("config.CombatTriggers.DrowningDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
                     if (s.getConfig().getBoolean("config.CombatTriggers.MobDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) {
                     if (s.getConfig().getBoolean("config.CombatTriggers.CreeperDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.FALL) {
                     if (s.getConfig().getBoolean("config.CombatTriggers.FallDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.FALLING_BLOCK) {
                     if (s.getConfig().getBoolean("config.CombatTriggers.FallingBlockDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.FIRE) {
                     if (s.getConfig().getBoolean("config.CombatTriggers.FireDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.FIRE_TICK) {
                     if (s.getConfig().getBoolean("config.CombatTriggers.FireDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if (c == EntityDamageEvent.DamageCause.LAVA) {
                     if(s.getConfig().getBoolean("config.CombatTriggers.LavaDamage")) {
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.LIGHTNING){
                     if(s.getConfig().getBoolean("config.CombatTriggers.LightningDamage")){
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.MAGIC){
                     if(s.getConfig().getBoolean("config.CombatTriggers.MagicDamage")){
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.MELTING){
                     if(s.getConfig().getBoolean("config.CombatTriggers.MeltingDamage")){
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.POISON){
                     if(s.getConfig().getBoolean("config.CombatTriggers.PoisonDamage")){
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.PROJECTILE){
                     if(s.getConfig().getBoolean("config.CombatTriggers.ProjectileDamage")){
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.STARVATION){
                     if(s.getConfig().getBoolean("config.CombatTriggers.HungerDamage")){
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.SUFFOCATION){
-                    if(s.getConfig().getBoolean("config.CombatTriggers.Suffocation")){
-                        core.inCombatVictim(p);
+                    if(s.getConfig().getBoolean("config.CombatTriggers.SuffocationDamage")){
+                        Core.inCombatVictim(p);
                     }
                 }
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.THORNS){
                     if(s.getConfig().getBoolean("config.CombatTriggers.ThornsDamage")){
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
+                Core.inCombatVictim(p);
             }
-            if(!core.combatTagTime.containsKey(p)) {
+            if(!Core.combatTagTime.containsKey(p)) {
                 if(c == EntityDamageEvent.DamageCause.WITHER){
                     if(s.getConfig().getBoolean("config.CombatTriggers.WitherDamage")){
-                        core.inCombatVictim(p);
+                        Core.inCombatVictim(p);
                     }
                 }
             }
         }
     }
 }
-
